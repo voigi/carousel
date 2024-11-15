549fdbd4087cd990697a3f6d7d8d95aa63f943e3
FrançoisB <supervoigi@gmail.com>
2024-11-15 17:29:31 +0100

    update script

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function showAlert() {
     
     message: 'Média ajouté au carrousel avec succès',
     position: { x: 'right', y: 'top' },
-    timeout: 13000,
+    timeout: 9000,
     icon: {
       className: 'fa fa-check',
       tagName: 'i'
