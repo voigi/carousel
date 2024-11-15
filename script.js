f1f21e9ebd1399cbeffcca3c791948e3ed4ed0f9
FrançoisB <supervoigi@gmail.com>
2024-11-15 07:37:04 +0100

    update script

--- script.js
+++ script.js
@@ -170,6 +170,7 @@ async function fetchSounds() {
   });
   audioFileInput.addEventListener("change", () => {
     console.log("Changement détecté dans audioFileInput");
+    console.log(audioFileInput.value);
     // activer confirmPreview
     confirmPreview.disabled = false;
     soundSelector.selectedIndex=0; // Réinitialise le selecteur de son
